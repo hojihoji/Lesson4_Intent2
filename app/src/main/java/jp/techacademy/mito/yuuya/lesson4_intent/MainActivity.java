7d9ef4545604516c95ea69621dc2908b0d90217d
yuuya.mito <yuya.m.tokai@gmail.com>
2017-12-24 18:22:35 +0900

    Initial commit (SecondActivity's setText does not work)

---
+++ app/src/main/java/jp/techacademy/mito/yuuya/lesson4_intent/MainActivity.java
@@ -0,0 +1,31 @@
+package jp.techacademy.mito.yuuya.lesson4_intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setText("セカンドアクティビティを開く");
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+
+        TextView textView =(TextView) findViewById(R.id.textView);
+        textView.setText("セカンドアクティビティを開きました");
+
+    }
+}
